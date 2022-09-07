828c12cecca704a7f2e845cf8472ab692718cad4
Nigless <nigless>
2022-09-07 14:46:53 +0000

    ...

--- src/camera.rs
+++ src/camera.rs
@@ -1,53 +1,45 @@
 use crate::control::Control;
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::Projection};
 use std::f32::consts::PI;
 
 #[derive(Component)]
-struct Cam {
-    target: Option<Entity>,
-}
+struct Camera();
+
+#[derive(Component)]
+struct CameraTarget();
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(startup)
-            .add_system(target)
-            .add_system(follow);
+        app.add_startup_system(startup).add_system(follow);
     }
 }
 
 fn startup(mut commands: Commands) {
     commands
-        .spawn_bundle(PerspectiveCameraBundle {
-            perspective_projection: PerspectiveProjection {
+        .spawn_bundle(Camera3dBundle {
+            projection: Projection::Perspective(PerspectiveProjection {
                 fov: PI * 0.4,
                 ..default()
-            },
+            }),
+            transform: Transform::from_xyz(0.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
             ..default()
         })
-        .insert(Cam { target: None });
+        .insert(Camera());
 }
 
-fn target(entity_q: Query<Entity, With<Control>>, mut camera_q: Query<&mut Cam>) {
-    let mut camera = camera_q.single_mut();
-    if let Some(_) = camera.target {
+fn follow(
+    mut camera_q: Query<&mut Transform, With<Camera>>,
+    mut target_q: Query<&Transform, (With<CameraTarget>, Without<Camera>)>,
+) {
+    if let Err(_) = target_q.get_single() {
         return;
     }
+    let target_t = target_q.single_mut();
 
-    if let Ok(entity) = entity_q.get_single() {
-        camera.target = Some(entity)
-    }
-}
+    let mut camera_t = camera_q.single_mut();
 
-fn follow(mut camera_q: Query<(&Cam, &mut Transform)>, target_q: Query<&Transform, Without<Cam>>) {
-    let (camera, mut transform) = camera_q.single_mut();
-
-    if let Some(target) = camera.target {
-        if let Ok(component) = target_q.get_component::<Transform>(target) {
-            transform.translation = component.translation;
-            transform.translation.y += 0.8;
-            transform.rotation = component.rotation
-        };
-    };
+    camera_t.translation = target_t.translation;
+    camera_t.rotation = target_t.rotation;
 }
