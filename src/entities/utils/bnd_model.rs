439df7d32a655ad8d1cda868c36800a1c74375b9
Nigless <nigless.gaming@gmail.com>
2022-11-05 00:29:53 +0600

    WithModel

--- src/entities/utils/bnd_model.rs
+++ src/entities/utils/bnd_model.rs
@@ -1,9 +1,9 @@
-use crate::model::Model;
+use crate::model::WithModel;
 use bevy::prelude::*;
 
 #[derive(Bundle)]
 pub struct BndModel {
-    model: Model,
+    model: WithModel,
     visibility: Visibility,
     computed_visibility: ComputedVisibility,
 }
@@ -11,7 +11,7 @@ pub struct BndModel {
 impl BndModel {
     pub fn new(src: &str) -> Self {
         Self {
-            model: Model::new(src),
+            model: WithModel::new(src),
             visibility: Default::default(),
             computed_visibility: Default::default(),
         }
