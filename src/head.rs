c180b54738f401cde705abb277acb1f99c7a1516
Nigless <nigless>
2022-11-08 01:02:07 +0600

    refactoring head.rs

--- src/head.rs
+++ src/head.rs
@@ -1,10 +1,10 @@
 use bevy::prelude::*;
 use bevy::{
     core::Name,
+    ecs::query::WorldQuery,
     hierarchy::Children,
-    prelude::{App, Commands, Component, Entity, Plugin, Query, With, World},
+    prelude::{App, Commands, Component, Entity, Plugin, Query, With},
 };
-use regex::Regex;
 
 #[derive(Component)]
 pub struct WithHead;
@@ -28,11 +28,19 @@ impl Plugin for HeadPlugin {
     }
 }
 
-fn resolve(world: &World, mut commands: Commands, entity: Query<Entity, With<WithHead>>) {
-    for entity in entity.iter() {
-        let head = get_head(world, entity, &Regex::new(r"#head").unwrap());
-        if let None = head {
-            return;
+fn resolve(
+    mut commands: Commands,
+    target_q: Query<(Entity, &Children), With<WithHead>>,
+    entity_q: Query<(Option<&Children>, Option<&Name>), Without<WithHead>>,
+) {
+    let label = "#head";
+
+    for (entity, children) in target_q.iter() {
+        let mut head = None;
+        for child in children.into_iter() {
+            if let Some(entity) = find(&entity_q, *child, &label) {
+                head = Some(entity)
+            }
         }
         commands
             .entity(entity)
@@ -41,17 +49,27 @@ fn resolve(world: &World, mut commands: Commands, entity: Query<Entity, With<Wit
     }
 }
 
-fn get_head(world: &World, entity: Entity, regex: &Regex) -> Option<Entity> {
-    for entity in world.get::<Children>(entity)?.into_iter() {
-        if let Some(name) = world.get::<Name>(*entity) {
-            if regex.is_match(name.as_str()) {
-                return Some(*entity);
-            }
+fn find<Q: WorldQuery>(
+    target_q: &Query<(Option<&Children>, Option<&Name>), Q>,
+    entity: Entity,
+    label: &str,
+) -> Option<Entity> {
+    let (children, name) = match target_q.get(entity) {
+        Ok(e) => e,
+        Err(_) => return None,
+    };
+
+    if let Some(_name) = name {
+        if _name.as_str().contains(label) {
+            return Some(entity);
         }
-        let head = get_head(world, *entity, regex);
-        if let Some(head) = head {
-            return Some(head);
+    };
+
+    for child in children?.into_iter() {
+        if let Some(result) = find(target_q, *child, label) {
+            return Some(result);
         }
     }
+
     None
 }
