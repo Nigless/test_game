528f78270a0daff3378aea7ec14fd0944efd8910
Nigless <nigless>
2022-11-07 19:47:13 +0000

    refactoring main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,29 +13,33 @@ mod entities;
 mod head;
 mod model;
 mod sprite;
+mod utils;
 use bevy_rapier3d::prelude::*;
 use camera::CameraTarget;
 use control::Control;
+use entities::bullet::Bullet;
 use entities::player::Player;
 use head::HeadPlugin;
 use model::ModelPlugin;
+use sprite::SpritePlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(ControlPlugin)
-        .add_plugin(CameraPlugin)
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugin(RapierDebugRenderPlugin::default())
-        .add_plugin(EditorPlugin)
-        .add_plugin(HeadPlugin)
-        .add_plugin(ModelPlugin)
         .insert_resource(AmbientLight {
             color: Color::rgb(1.0, 1.0, 1.0),
             brightness: 0.9,
         })
         .insert_resource(ClearColor(Color::rgb(0.8, 0.8, 0.8)))
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(startup)
+        .add_plugin(EditorPlugin)
+        .add_plugin(CameraPlugin)
+        .add_plugin(ControlPlugin)
+        .add_plugin(HeadPlugin)
+        .add_plugin(ModelPlugin)
+        .add_plugin(SpritePlugin)
         .run();
 }
 
@@ -43,7 +47,6 @@ fn startup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    server: Res<AssetServer>,
 ) {
     commands
         .spawn_bundle(Player::new())
@@ -63,17 +66,18 @@ fn startup(
     //     .insert(RigidBody::Dynamic)
     //     .insert(Collider::cuboid(0.3, 0.3, 0.3));
 
+    let mesh = meshes.add(Mesh::from(shape::Plane { size: 1.0 }));
+    let material = materials.add(StandardMaterial {
+        base_color: Color::BLACK,
+        reflectance: 0.0,
+        unlit: false,
+        ..Default::default()
+    });
     for x in -5..5 {
         for z in -5..5 {
             commands.spawn_bundle(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Plane { size: 1.0 })),
-
-                material: materials.add(StandardMaterial {
-                    base_color: Color::GREEN,
-                    reflectance: 0.0,
-                    unlit: false,
-                    ..Default::default()
-                }),
+                mesh: mesh.clone(),
+                material: material.clone(),
                 transform: Transform::from_translation(Vec3::new(
                     x as f32 * 5.0,
                     0.0,
