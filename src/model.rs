44f838a62b86a6d18c7c38bbb5c5fd8e159b023d
Nigless <nigless>
2025-01-21 19:02:49 +0000

    deserialize components

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,21 @@
 use bevy::{
-    ecs::component::{ComponentHooks, StorageType},
+    ecs::{
+        component::{ComponentHooks, StorageType},
+        reflect::ReflectCommandExt,
+    },
     prelude::*,
+    ptr::OwningPtr,
+    reflect::{
+        serde::{ReflectDeserializer, TypedReflectDeserializer},
+        TypeData, TypeRegistration,
+    },
 };
-use bevy_rapier3d::geometry::{Collider, ComputedColliderShape};
+use bevy_rapier3d::{
+    geometry::{Collider, ComputedColliderShape},
+    prelude::{TriMeshFlags, VHACDParameters},
+};
+use serde::de::DeserializeSeed;
+use serde_json::{Deserializer, Value};
 
 pub struct Model {
     pub src: String,
@@ -53,6 +66,7 @@ impl Plugin for ModelPlugin {
 
 fn resolve(
     meshes_res: Res<Assets<Mesh>>,
+    types_res: Res<AppTypeRegistry>,
     mut commands: Commands,
     models_q: Query<(Entity, &Children, &GltfExtras)>,
     meshes_q: Query<&Mesh3d>,
@@ -60,11 +74,45 @@ fn resolve(
     for (entity, children, extras) in models_q.iter() {
         commands.entity(entity).remove::<GltfExtras>();
 
-        if extras.value != "{\"collider\":true}" {
+        let Ok(json_value) = serde_json::from_str::<Value>(&extras.value) else {
+            continue;
+        };
+
+        let Some(extras) = json_value.as_object() else {
+            continue;
+        };
+
+        let types = types_res.read();
+
+        for (component_name, value) in extras.clone() {
+            let Some(component_type) = types.get_with_type_path(&component_name).cloned() else {
+                continue;
+            };
+
+            let Some(params) = value.as_str() else {
+                continue;
+            };
+
+            let reflect_deserializer = TypedReflectDeserializer::new(&component_type, &types);
+
+            let mut deserializer = Deserializer::from_str(params);
+
+            let value = reflect_deserializer.deserialize(&mut deserializer).unwrap();
+
+            commands.entity(entity).insert_reflect(value);
+        }
+
+        if !extras
+            .get("collider")
+            .and_then(|v| v.as_bool())
+            .unwrap_or(false)
+        {
             continue;
         }
 
         let mesh = meshes_q.get(children[0]).unwrap();
+
+        let mesh = meshes_res.get(mesh).unwrap();
 
         commands.entity(children[0]).despawn();
 
@@ -72,8 +120,8 @@ fn resolve(
             .entity(entity)
             .insert(
                 Collider::from_bevy_mesh(
-                    meshes_res.get(mesh).unwrap(),
-                    &ComputedColliderShape::default(),
+                    mesh,
+                    &ComputedColliderShape::TriMesh(TriMeshFlags::all()),
                 )
                 .unwrap(),
             )
